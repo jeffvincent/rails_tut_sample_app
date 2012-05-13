9aa2a98ab4d1cfaa049322449a0efa0d1eb5e143
Jeff Vincent <jeff@wistia.com>
2012-05-13 00:23:11 -0400

    just did some crazy routes stuff post Chap 5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'e4973ebcbdc2b20129315aff3cd362e6fbb94555c33b98e711f0c9db746df11fa91e1723115bfb8e02a616919557644c2d00e42994cffc9b106f0625cb24d09c'
+SampleApp::Application.config.secret_token = '185fda29a59659171a84fcd02689b3d8f581eee4a3ec867baf7f909ec8e5baeae82ed71db4e7f2f991d7cf95b0b8ce2ca908e23d29a3e5fda85bacb263b21d4a'
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 SampleApp::Application.routes.draw do
   
-  get "users/new"
+  get 'users/new'
 
-  root to: 'static_pages#home' 
-
-  match '/help', to: 'static_pages#help'
+  root to: 'static_pages#home'
 
   match '/about', to: 'static_pages#about'
 
   match '/contact', to: 'static_pages#contact'
 
-  match '/signup', to: 'users#new'
+  match '/help', to: 'static_pages#help'
+
+  match '/signup', to: 'users#signup'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,11 +61,11 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id))(.:format)'
 end
